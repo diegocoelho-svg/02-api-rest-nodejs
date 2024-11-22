332ccce670489de391bf7b33195e979eaac3323e
Diego Coelho <diegocoelhoaprova@gmail.com>
2024-11-22 08:19:48 -0300

    configurando o knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-// GET, POST, PUT, PATCH, DELETE
-//http://localhost:3333/hello // http localhost:3333/hello
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app.listen({
