9223471c29558505a7e0b4a061e96c91fb50d063
Diego Coelho <diegocoelhoaprova@gmail.com>
2024-11-26 08:42:59 -0300

    Criação de Transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,13 +1,30 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('Amount', 1000)
-      .select('*') //selecionar todas as transações criadas
+  app.post('/', async (request, reply) => {
+  //title, amount, type: credit or debit
+    const createTransactionBodySchema = z.object({
+    title: z.string(),
+    amount: z.number(),
+    type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body) //validando os dados para ver se bate com o schema
+
   
-    return transactions
+     await knex ('transactions')
+    .insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    //http codes -> 201, recurso criado com sucesso
+
+    return reply.status(201).send()
   })
 }
 
