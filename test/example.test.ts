03527567a6ba25368e3767fd43438d63150540d8
Diego Coelho <diegocoelhoaprova@gmail.com>
2024-11-29 08:01:32 -0300

    Testando e Criação de transação

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,8 +1,24 @@
-import { expect, test } from 'vitest'
+import { expect, test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app' //vai tentar subir um servidor na porta 3333
 
-test('o usuário consegue criar uma nova transação', () => {
+beforeAll(async () => {
+  await app.ready()
+}) //executa antes de todos os testes -> Each
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
   // fazer a chamada http p/ criar uma nova transação
-  const responseStatusCode = 201
+  const response = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    })
 
-  expect(responseStatusCode).toEqual(201)
+  expect(201) // -> valida se o retorno foi um status 201
 })
