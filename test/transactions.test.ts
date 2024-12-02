4d536c102340bc0e216db2434579bc98186ae20c
Diego Coelho <diegocoelhoaprova@gmail.com>
2024-12-02 18:31:18 -0300

    listagem de transações pt 2

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app' //vai tentar subir um servidor na porta 3333
 
@@ -11,23 +11,39 @@ describe('Transactions routes', () => {
     await app.close()
   })
   
-  it.only('should be able to create a new transaction', async () => {
-    // fazer a chamada http p/ criar uma nova transação
-    const response = await request(app.server)
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
       .post('/transactions')
       .send({
         title: 'New Transaction',
         amount: 5000,
         type: 'credit',
       })
-  
-      .expect(201) // -> valida se o retorno foi um status 201
-
-    console.log(response.headers) // ou pode ser console.log(response.get('Set-cookie'))
+      .expect(201)
   })
 
-  it.skip('should be able to list all transactions', async () => {
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+    
+    const cookies = createTransactionResponse.get('Set-Cookie')
 
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      })
+    ])
   })
 
 })
