3203a138f207c5bb91f963d40858738462f111d4
Diego Coelho <diegocoelhoaprova@gmail.com>
2024-12-02 18:03:07 -0300

    Categorizando os Testes

---
+++ test/transactions.test.ts
@@ -0,0 +1,28 @@
+import { test, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app' //vai tentar subir um servidor na porta 3333
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  }) //executa antes de todos os testes -> Each
+  
+  afterAll(async () => {
+    await app.close()
+  })
+  
+  test('User can create a new transaction', async () => {
+    // fazer a chamada http p/ criar uma nova transação
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+  
+      .expect(201) // -> valida se o retorno foi um status 201
+  })
+
+})
+
